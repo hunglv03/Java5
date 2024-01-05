f991ea1d60ce797bbb273f47568c1c2ff9b16bbd
hunglv03 <Lvh9x8z5102003@gmail.com>
2024-01-06 00:37:35 +0700

    Buổi 2
    ok

---
+++ java5/src/main/java/com/example/java5/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.java5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home() {
+        return "index";
+    }
+}
